7e9374dcf27d75a8ff68ac31ef899d61b814f95d
Katarzyna Polakowska <nephente@interia.pl>
2018-11-16 15:27:18 +0100

    handled errors

--- src/App/List.tsx
+++ src/App/List.tsx
@@ -7,9 +7,21 @@ interface RowDataPropsType {
     rowAmountPln:number,
     rowAmountEur:number | string, 
     rowID:number,
-    key: number,
     list: Array<Row>,
     handleDelete:(id: number) => void
+}
+
+interface ErrorsPropsType {
+   message: string,
+}
+
+@observer
+class Errors extends React.Component<ErrorsPropsType> {
+    render(){
+        return(
+            <div className="errors">{this.props.message}</div>
+        )
+    }
 }
 
 @observer 
@@ -37,72 +49,55 @@ interface Row {
     amountPLN: number,
 }
 
-/*interface ArrayPln {
-    inputPln: number,
-    idPln: number
-}*/
-
 @observer
 export class List extends React.Component {
-    
     @observable inputEur:number = 4.382;
     @observable inputPln:number = 0;
-    //@observable sumEur:number = 0;
-    //@observable sumPln:number = 0;
-    //@observable arrayEur:Array<number> = [];
-    //@observable arrayPln: Array<ArrayPln> = [];
     @observable inputTitle: string = '';
-    //@observable arrayTitle:Array<string> = [];
-    //@observable sumAllEuro:number = 0;
 
-    @observable list: Array<Row>
+    @observable list: Array<Row>;
     @observable licznik: number = 0;
+    @observable errors: Array<string> = [];
+    @observable komunikat: string = '';
 
     constructor(props:Row) {
         super(props);
         this.list = [];
     }
 
+
     handleClick = () => {
-        this.list.push({
-            title: this.inputTitle,
-            amountPLN: this.inputPln,
-            id: this.licznik++
-        });
-
-        ///this.arrayEur.push(this.inputEur);
-        //this.sumEur = this.arrayEur.reduce((a , b )=> a + b, 0);
-
-        //this.arrayTitle.push(this.inputTitle);
-
-        /*this.arrayPln.push({
-            inputPln: this.inputPln,
-            idPln: this.licznik++
-        });*/
-            
-        //this.sumPln=this.arrayPln.map(x => x.inputPln).reduce((a , b )=> a + b, 0);
-        //this.sumaPln();
+        this.errors = [];
         
-        //console.log('this.sumPln',this.sumPln)
-        
+    //validation conditions
+        if(this.inputTitle.length < 5){
+            this.errors.push("Title should have at least 5 characters");
+        } else if(this.inputPln < 1){
+            this.errors.push("Type amount in PLN");
+        } else {
+            console.log('clear')
+            this.list.push({
+                title: this.inputTitle,
+                amountPLN: this.inputPln,
+                id: this.licznik++
+            });
+
+            this.inputPln = 0;
+            this.inputTitle = '';
+        }
     }
 
     @computed get sumaPln() {
         let sum = 0;
         for (const item of this.list) {
             sum = sum + item.amountPLN;
-        }
-        return sum;
+        } return sum;
         //return this.arrayPln.map(x => x.inputPln).reduce((a , b )=> a + b, 0);
     }
 
     @computed get sumAllEuro() {
         return parseFloat( (this.sumaPln / this.inputEur).toFixed(2) );
     }
-
-    // sumaEuro = () => {
-    // this.sumAllEuro = parseFloat( (this.sumPln / this.inputEur).toFixed(2) );
-    // }
 
     handleChangeValue = (e: React.FormEvent<HTMLInputElement>) => {
         let eurValFromInput = e.currentTarget.value;
@@ -113,7 +108,7 @@ export class List extends React.Component {
         this.inputTitle = e.currentTarget.value;
     }
 
-    handleChangeAmount = (e: React.FormEvent<HTMLInputElement>) =>{
+    handleChangeAmount = (e: React.FormEvent<HTMLInputElement>) => {
         let plnValFromInput = e.currentTarget.value;
         this.inputPln = parseFloat(plnValFromInput);
     }
@@ -125,7 +120,7 @@ export class List extends React.Component {
         return amountPLN / this.inputEur;
     }
 
-    renderRow = (row: Row) => {
+    renderRow = (row: Row, id: number) => {
         const rowID = row.id;
         return(
             <RowData 
@@ -135,166 +130,58 @@ export class List extends React.Component {
                 rowAmountEur={this.renderEuro(row.amountPLN).toFixed(2)} 
                 rowID={rowID}
                 handleDelete={this.handleDelete}
-                key={rowID} />)
+                key={id}
+            />)
     };
 
     handleDelete = (idToDelete: number ) => {
-        const newList = this.list.filter(  //modyfikuje
-            (item) => item.id !== idToDelete
+        const newList = this.list.filter( (item) => item.id !== idToDelete );
+        this.list = newList;
+    }
+
+    renderError() {
+        if(this.errors.length > 0) {
+            return this.errors.map(
+                (el, id) => <Errors message={el} key={id}/>
             );
-        this.list = newList; //zwracam nowa liste
-        
-        //const newArrayPln = newList.filter( (el) => el.id !== idToDelete);
-        
-        //this.sumaPln();
-        
-        //this.sumPln = newArrayPln.map( (el) => el.amountPLN ).reduce((a , b )=> a + b, 0);
-        //console.log('del',this.sumPln )
-        //this.sumAllEuro = parseFloat( (this.sumPln / this.inputEur).toFixed(2) );
-        
-        console.log('this.sumAllEuro',this.sumAllEuro)
+        }
     }
 
     render(){
-        
         return (
-        <div>
-        <div className="inputs">
-            <h1>List of expenses</h1>
-            <div className="error-message"></div>
-            <label>Type value: 1 EUR
-            <input onChange={this.handleChangeValue} type="number" min="0" value={this.inputEur} />
-            </label>
-             
-            <label>Title of transaction
-            <input onChange={this.handleChangeTitle} type="text" value={this.inputTitle}/>
-            </label>
-            <label>Amount ( in PLN )
-            <input onChange={this.handleChangeAmount} type="number" min="0" />
-            </label> 
-            <button onClick={this.handleClick}>ADD</button>
-        </div>
-        <div className="table">
-            <div className="tableHeader">
-                <div className="title">Title</div>
-                <div className="amountPLNHead">Amount (PLN)</div>
-                <div className="amountEURHead">Amount (EUR)</div>
-                <div className="optionCell">Options</div>
-            </div>
-        </div>  
-            <div>  
-                { this.renderList()  }
-            </div> 
-        <h2>{this.sumAllEuro}</h2>
-            
-    </div>
-        )   
-    }
+            <div>
+                <div className="inputs">
+                    <h1>List of expenses</h1>
 
+                    { this.renderError() }
+                    
+                    <label>Type value: 1 EUR
+                    <input onChange={this.handleChangeValue} type="number" min="0" value={this.inputEur} />
+                    </label>
+                    
+                    <label>Title of transaction
+                    <input onChange={this.handleChangeTitle} type="text" value={this.inputTitle}/>
+                    </label>
+                    <label>Amount ( in PLN )
+                    <input onChange={this.handleChangeAmount} type="number" value={this.inputPln} />
+                    </label> 
+                    <button onClick={this.handleClick}>ADD</button>
+                </div>
+                <div className="table">
+                    <div className="tableHeader">
+                        <div className="title">Title</div>
+                        <div className="amountPLNHead">Amount (PLN)</div>
+                        <div className="amountEURHead">Amount (EUR)</div>
+                        <div className="optionCell">Options</div>
+                    </div>
+                </div>  
+                    <div>  
+                        { this.renderList()  }
+                    </div> 
+                <h2>{this.sumAllEuro}</h2>
+            </div>
+        );
+    }
 }
 
 export default List;
-
- //handleClick = () => {     
-                                        //TODO
-        /*
-        let errors = [];
-
-        //validation conditions
-        if(this.state.inputTitle.length < 5){
-            errors.push("Title should have at least 5 characters");
-            console.log(errors);
-        } else if(this.state.inputPln.length < 1){
-            errors.push("Type amount in PLN");
-            console.log(errors)
-        }
-
-        if(errors.length > 0) {
-            var errorMessage = document.querySelector('.error-message');       
-             for(let i=0; i<errors.length; i++) {
-                 errorMessage.innerHTML += errors[i] + '<br/>'
-                 errorMessage.style.color='red';
-                }
-        } else {
-
-            //clear errors messages
-        document.querySelector('.error-message').innerHTML = '';
-
-            //clear inputs after click
-        this.setState({
-            inputTitle: '',
-            inputPln: ''
-        })
-
-        let row = document.createElement("div");
-        row.className = "rows";
-
-        let divTitle = document.createElement("div");
-        divTitle.className = "title";
-        row.appendChild(divTitle);
-        divTitle.innerHTML = this.state.inputTitle;
-
-        let divAmount = document.createElement("div");
-        divAmount.className = "amountPLN";
-        row.appendChild(divAmount);
-        let plnSum = (this.state.inputPln * 1).toFixed(2);
-        divAmount.innerHTML = plnSum;
-
-        let  amountEUR = document.createElement("div");
-        amountEUR.className = "amountEUR";
-        row.appendChild(amountEUR);
-        let euroSum = (this.state.inputPln / this.state.eurVal).toFixed(2);
-        amountEUR.innerHTML = euroSum;
-
-        let deleteBtn = document.createElement("button");
-        deleteBtn.className = "deleteButton";
-        row.appendChild(deleteBtn);
-        deleteBtn.innerText = "delete";
-
-        document.querySelector('.table').appendChild(row);
-        
-        //counting the sum of PLN
-        let sumPLN = document.querySelectorAll('.amountPLN');
-        let arr = [];
-        for(let i = 0; i < sumPLN.length; i++){
-            arr.push(parseFloat(sumPLN[i].innerText))
-            console.log("tablica: "+arr);
-        }
-
-        let stateSums = [...arr];
-        console.log("stateSums: "+stateSums);
-        this.setState({
-            sum: stateSums.reduce((a,b)=>a+b, 0)
-        })
-        
-        //counting the sum of EUR
-        let sumEUR = document.querySelectorAll('.amountEUR');
-        let arr2 = [];
-        for(let i = 0; i < sumEUR.length; i++){
-            arr2.push(parseFloat(sumEUR[i].innerText))
-        }
-
-        let stateSumsEur = [...arr2];
-        this.setState({
-            sumEur: stateSumsEur.reduce((a,b)=>a+b, 0).toFixed(2)
-        })
-
-        //delete row by button
-        deleteBtn.addEventListener('click', () => {
-            document.querySelector('.table').removeChild(row);
-            stateSums.pop();
-            stateSumsEur.pop()
-                this.setState({
-                    sum: stateSums.reduce((a,b)=>a+b, 0),
-                    sumEur: stateSumsEur.reduce((a,b)=>a+b, 0).toFixed(2)
-                })    
-              })  
-          }
-          */
-       // } //close if statement
-    //    renderRow = () => {
-    //     const a = this.arrayEur.map((el, i) =>
-    //     <div className="amountEUR">
-    //         <li key = { `el-${i}`}> {el} </li>
-    //     </div>
-    //     );}
